41ae796ad2d1203e190947b735a2cce86a014c42
team1091 <test@test.com>
2016-02-13 13:29:50 -0600

    Lets find the stuff

--- src/org/usfirst/frc/team1091/robot/Robot.java
+++ src/org/usfirst/frc/team1091/robot/Robot.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Solenoid;
 public class Robot extends SampleRobot {
 
 	CameraServer server;
@@ -25,14 +26,18 @@ public class Robot extends SampleRobot {
 	final Joystick leftJoy; // left joystick controller
 	final Joystick rightJoy; // right joystick controller
 	
+	
 	final Victor lShoot;
 	final Victor rShoot;
 	final Victor lift;
 	DigitalInput limit;
 	
 	Encoder lEncod; //20 per rotation
-	Encoder rEncod;
+	Encoder rEncod; //20 per rotation
 	Encoder liftEncod;
+	
+	Solenoid in;
+	Solenoid out;
 	
 
 	final double deadZone = 0.02;
@@ -84,6 +89,9 @@ public class Robot extends SampleRobot {
 		lEncod = new Encoder(1, 2, true);
 		rEncod = new Encoder(3, 4);
 		liftEncod = new Encoder(5, 6);
+		
+		in = new Solenoid(0);
+		out = new Solenoid(1);
 		
 		xbox = new Joystick(0);
 		cyborg = new Joystick(1);
@@ -264,14 +272,15 @@ public class Robot extends SampleRobot {
 		long currentTime = System.currentTimeMillis();
 		long changeTime = currentTime - lastTime;
 		
-		long lCurrentEncod = lEncod.get();
-		long lChangeEncod = lCurrentEncod - lLastEncoderVal;
+		long lCurrentEncod = Math.abs(lEncod.get());
+		double lChangeEncod = (double)lCurrentEncod - lLastEncoderVal;
 		
-		long rCurrentEncod = rEncod.get();
-		long rChangeEncod = rCurrentEncod - rLastEncoderVal;
+		long rCurrentEncod = Math.abs(rEncod.get());
+		double rChangeEncod = (double)rCurrentEncod - rLastEncoderVal;
 		
-		long lCurrentRPM = (lChangeEncod) / (changeTime * 1200000); 
-		long rCurrentRPM = (rChangeEncod) / (changeTime * 1200000);
+		long lCurrentRPM = (long)(((lChangeEncod / 20) / (changeTime)) * 60000); 
+		long rCurrentRPM = (long)(((rChangeEncod / 20) / (changeTime)) * 60000);
+		
 		// Figure out what we are getting from serial
 //		byte[] data = serialPort.read(serialPort.getBytesReceived());
 //
@@ -292,7 +301,9 @@ public class Robot extends SampleRobot {
 		xboxDrive(); // For xbox controls
 		xboxShoot(); // For xbox shooting
 		cyborgDrive(); // For cyclops controls
-		joyDrive(); // For dual joystick controls
+//		cyborgShoot(); //For cyclops shooting
+//		joyDrive(); // For dual joystick controls
+//		joyShoot(); //For dual joystick shooting
 		
 		lastTime = currentTime;
 		lLastEncoderVal = lEncod.get();
@@ -308,7 +319,7 @@ public class Robot extends SampleRobot {
 			Timer.delay(0.001); // wait for a motor update time
 		}
 	}
-	//XBOX SHOOTING CONTROLS
+	//XBOX SHOOTING CONTROLS 
 	private void xboxShoot() {
 		if (xboxBut1) // Right Joy Arcade Drive
 		{
@@ -317,22 +328,25 @@ public class Robot extends SampleRobot {
 			
 			if(!(Math.abs(trigger) < deadZone))
 			{
-				lShoot.set(trigger);
-				rShoot.set(-trigger);
+				lShoot.set(-trigger);
+				rShoot.set(trigger);
+				//launch solenoid
 			}
 			else if(xbox.getRawButton(5) == true){
 				double var = 0.5;
-				lShoot.set(-var);
-				rShoot.set(var);
+				lShoot.set(var);
+				rShoot.set(-var);
 			}
 			else {
 				lShoot.set(0);
 				rShoot.set(0);
 			}
-			if(!(Math.abs(yAxis) < deadZone))
-			lift.set(yAxis * -0.50);
+			if(!(Math.abs(yAxis) < deadZone) && limit.get())
+				lift.set(yAxis * -0.50);
+			else if(!(Math.abs(yAxis) < deadZone) && yAxis > 0)
+				lift.set(yAxis * -0.50);
 			else 
-			lift.set(0.4);
+				lift.set(0.4);
 				
 		}
 		if (xboxBut2) // Left Joy Arcade Drive
@@ -342,22 +356,24 @@ public class Robot extends SampleRobot {
 			
 			if(!(Math.abs(trigger) < deadZone))
 			{
-				lShoot.set(trigger);
-				rShoot.set(-trigger);
+				lShoot.set(-trigger);
+				rShoot.set(trigger);
 			}
 			else if(xbox.getRawButton(6) == true){
 				double var = 0.5;
-				lShoot.set(-var);
-				rShoot.set(var);
+				lShoot.set(var);
+				rShoot.set(-var);
 			}
 			else {
 				lShoot.set(0);
 				rShoot.set(0);
 			}
-			if(!(Math.abs(yAxis) < deadZone))
-			lift.set(yAxis * -0.50);
+			if(!(Math.abs(yAxis) < deadZone) && limit.get())
+				lift.set(yAxis * -0.50);
+			else if(!(Math.abs(yAxis) < deadZone) && yAxis > 0)
+				lift.set(yAxis * -0.50);
 			else 
-			lift.set(0.4);
+				lift.set(0.4);
 		}
 		if (xboxBut3) // Joystick Tank Drive
 		{
@@ -367,12 +383,12 @@ public class Robot extends SampleRobot {
 			
 			if(!(Math.abs(lTrigger) < deadZone))
 			{
-				lShoot.set(lTrigger);
-				rShoot.set(-lTrigger);
+				lShoot.set(-lTrigger);
+				rShoot.set(lTrigger);
 			}
 			else if(!(Math.abs(rTrigger) < deadZone)){
-				lShoot.set(-rTrigger);
-				rShoot.set(rTrigger);
+				lShoot.set(rTrigger);
+				rShoot.set(-rTrigger);
 			}
 			else {
 				lShoot.set(0);
@@ -411,10 +427,10 @@ public class Robot extends SampleRobot {
 		}
 
 	}
-	//CYBORG SHOOTING CONTROLS
-	private void cyborgShoot() {
-		
-	}
+//	//CYBORG SHOOTING CONTROLS
+//	private void cyborgShoot() {
+//		
+//	}
 	//CYBORG DRIVING CONTROLS
 	private void cyborgDrive() {
 		if (cyborgBut1) // Joystick drive / lever shooter
@@ -439,42 +455,42 @@ public class Robot extends SampleRobot {
 				myRobot.arcadeDrive(yAxis, xAxis, true);
 		}
 	}
-	//DUAL JOYSTICK SHOOTING CONTROLS
-	private void joyShoot() {
-		
-	}
-	//DUAL JOYSTICK CONROLS
-	private void joyDrive() {
-		if (joyBut1) // Dual stick drive / hat shooter
-		{
-			double leftY = driveConvert(leftJoy.getRawAxis(1) * -1) * setSensitivity(rightJoy.getRawAxis(3)) * 0.5;
-			double rightY = driveConvert(rightJoy.getRawAxis(1) * -1) * setSensitivity(rightJoy.getRawAxis(3)) * 0.5;
-			if (!(Math.abs(rightY) < deadZone) || !(Math.abs(leftY) < deadZone)) // deadZone
-				myRobot.tankDrive(leftY, rightY, true);
-		}
-		if (joyBut2) // Dual stick drive / side button shooter
-		{
-			double leftY = driveConvert(leftJoy.getRawAxis(1) * -1) * setSensitivity(rightJoy.getRawAxis(3)) * 0.5;
-			double rightY = driveConvert(rightJoy.getRawAxis(1) * -1) * setSensitivity(rightJoy.getRawAxis(3)) * 0.5;
-			if (!(Math.abs(rightY) < deadZone) || !(Math.abs(leftY) < deadZone))
-				myRobot.tankDrive(leftY, rightY, true);
-		}
-		if (joyBut3) // Left stick drive / Right shooter
-		{
-			double leftY = driveConvert(leftJoy.getRawAxis(1) * -1) * setSensitivity(leftJoy.getRawAxis(3)) * 0.5;
-			double leftX = driveConvert(leftJoy.getRawAxis(2)* -1) * setSensitivity(leftJoy.getRawAxis(3)) * 0.5; //twist
-			if (!(Math.abs(leftX) < deadZone) || !(Math.abs(leftY) < deadZone))
-				myRobot.arcadeDrive(leftY, leftX, true);
-		}
-		if (joyBut4) // Right stick drive / Left shooter
-		{
-			double rightY = driveConvert(rightJoy.getRawAxis(1) * -1) * setSensitivity(rightJoy.getRawAxis(3)) * 0.5;
-			double rightX = driveConvert(rightJoy.getRawAxis(2) * -1) * setSensitivity(rightJoy.getRawAxis(3)) * 0.5; //twist
-			if (!(Math.abs(rightY) < deadZone) || !(Math.abs(rightX) < deadZone))
-				myRobot.arcadeDrive(rightY, rightX, true);
-		}
-	}
-
-	
-	public void test() {}
+//	//DUAL JOYSTICK SHOOTING CONTROLS
+//	private void joyShoot() {
+//		
+//	}
+//	//DUAL JOYSTICK CONROLS
+//	private void joyDrive() {
+//		if (joyBut1) // Dual stick drive / hat shooter
+//		{
+//			double leftY = driveConvert(leftJoy.getRawAxis(1) * -1) * setSensitivity(rightJoy.getRawAxis(3)) * 0.5;
+//			double rightY = driveConvert(rightJoy.getRawAxis(1) * -1) * setSensitivity(rightJoy.getRawAxis(3)) * 0.5;
+//			if (!(Math.abs(rightY) < deadZone) || !(Math.abs(leftY) < deadZone)) // deadZone
+//				myRobot.tankDrive(leftY, rightY, true);
+//		}
+//		if (joyBut2) // Dual stick drive / side button shooter
+//		{
+//			double leftY = driveConvert(leftJoy.getRawAxis(1) * -1) * setSensitivity(rightJoy.getRawAxis(3)) * 0.5;
+//			double rightY = driveConvert(rightJoy.getRawAxis(1) * -1) * setSensitivity(rightJoy.getRawAxis(3)) * 0.5;
+//			if (!(Math.abs(rightY) < deadZone) || !(Math.abs(leftY) < deadZone))
+//				myRobot.tankDrive(leftY, rightY, true);
+//		}
+//		if (joyBut3) // Left stick drive / Right shooter
+//		{
+//			double leftY = driveConvert(leftJoy.getRawAxis(1) * -1) * setSensitivity(leftJoy.getRawAxis(3)) * 0.5;
+//			double leftX = driveConvert(leftJoy.getRawAxis(2)* -1) * setSensitivity(leftJoy.getRawAxis(3)) * 0.5; //twist
+//			if (!(Math.abs(leftX) < deadZone) || !(Math.abs(leftY) < deadZone))
+//				myRobot.arcadeDrive(leftY, leftX, true);
+//		}
+//		if (joyBut4) // Right stick drive / Left shooter
+//		{
+//			double rightY = driveConvert(rightJoy.getRawAxis(1) * -1) * setSensitivity(rightJoy.getRawAxis(3)) * 0.5;
+//			double rightX = driveConvert(rightJoy.getRawAxis(2) * -1) * setSensitivity(rightJoy.getRawAxis(3)) * 0.5; //twist
+//			if (!(Math.abs(rightY) < deadZone) || !(Math.abs(rightX) < deadZone))
+//				myRobot.arcadeDrive(rightY, rightX, true);
+//		}
+//	}
+//
+//	
+//	public void test() {}
 }
